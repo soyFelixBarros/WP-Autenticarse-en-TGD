10a0772f4ac9104bff54b07f82117e628056b9a1
Felix Barros <soyfelixbarros@gmail.com>
2019-06-26 15:29:12 -0300

    version beta

--- inc/classes/Plugin.php
+++ inc/classes/Plugin.php
@@ -25,10 +25,14 @@ class Plugin
 
         if ( !empty( $code ) ) {
             $tgd = new Services\TGDService( $code );
-
-            $user = $tgd->getUserByCode( $code );
-
-            var_dump( $user );die;
+            $user_id = $tgd->getUserByCode( $code );
+            $user = get_user_by( 'id', $user_id ); 
+            
+            if ( $user ) {
+                wp_set_current_user( $user_id, $user->user_login );
+                wp_set_auth_cookie( $user_id );
+                do_action( 'wp_login', $user->user_login );
+            }
         //     // wp_redirect( home_url() );
         //     // exit;
         }
--- inc/classes/Services/TGDService.php
+++ inc/classes/Services/TGDService.php
@@ -46,6 +46,43 @@ class TGDService
 
         $this->apiClient->setAccessToken($tokenInfo['access_token']);
 
-        return $this->apiClient->callApi('persona');
+        $personaInfo = $this->apiClient->callApi('persona');
+
+        $user = $this->saveOrUpdateUser($personaInfo, $tokenInfo);
+
+        return $user;
+    }
+
+   /**
+     * Crea o actualiza el usuario con los datos de la persona y los del token.
+     *
+     * Se deben seleccionar los datos que vienen de la petición de la api de persona.
+     * Lo importante es guardar siempre el id de la persona en tgd.
+     */
+    protected function saveOrUpdateUser($personaInfo, $tokenInfo)
+    {
+        // Busco si el usuario existe en WordPress
+        $user_email = $personaInfo['emails'][0]['email'];
+        $cuil = $personaInfo['cuitCuil'];
+        $user_id = username_exists( $cuil );
+
+        if ( !$user_id and email_exists($user_email) == false ) {
+            $user_id = wp_create_user(
+                $cuil, // username
+                NULL,//$personaInfo['cuitCuil'], // password
+                $user_email // correo
+            );
+        }
+        
+        $user_id = wp_update_user(
+            array(
+                'ID'         => $user_id,
+                'nickname'   => $user_email,
+                'first_name' => $personaInfo['apellidos'],
+                'last_name'  => $personaInfo['nombres'],
+            )
+        );
+
+        return $user_id;
     }
 }
