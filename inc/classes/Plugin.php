7ee12603c8dc79f119121f414d5a43640bf77387
Felix Barros <soyfelixbarros@gmail.com>
2019-06-27 12:14:25 -0300

    refactorizacion

--- inc/classes/Plugin.php
+++ inc/classes/Plugin.php
@@ -28,13 +28,14 @@ class Plugin
             $user_id = $tgd->getUserByCode( $code );
             $user = get_user_by( 'id', $user_id ); 
             
+            // Inicio de sesión de usuario
             if ( $user ) {
-                var_dump($user);
                 wp_set_current_user( $user_id, $user->user_login );
                 wp_set_auth_cookie( $user_id );
                 do_action( 'wp_login', $user->user_login );
             }
 
+             // Redireccionamos a la pagina principal
             wp_redirect( home_url() );
             exit;
         }
